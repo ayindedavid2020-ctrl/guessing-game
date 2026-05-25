8486bfe8b80b4fce5a51e5602f0526c6f6b19415
ayindedavid2020-ctrl <ayinde.david2020@gmail.com>
2026-05-26 08:31:03 +1300

    Add more tries
    I updated the code to allow more tries

--- guessingGame.py
+++ guessingGame.py
@@ -1,16 +1,26 @@
-#code version 1
+#code version 2
 import random
 
 def play_game():
     print("--- Welcome to the Number Guessing Game! ---")
+    print("\nYou have to guess a number between 1 and 10 both inclusive")
     secret_number = random.randint(1, 10)
+    attempts = 3
     
-    guess = int(input("Guess a number between 1 and 10: "))
+    print(f"You have {attempts} attempts to guess the correct number.\n")
     
-    if guess == secret_number:
-        print("Amazing! You got it right on the first try!")
+    for i in range(attempts):
+        guess = int(input(f"Attempt {i+1}: Enter your guess: "))
+        
+        if guess == secret_number:
+            print("\nAmazing! You got it right!")
+            break
+        elif guess < secret_number:
+            print("\nToo low!")
+        else:
+            print("\nToo high!")
     else:
-        print(f"Oops, wrong guess! The number was {secret_number}.")
+        print(f"Game over! The correct number was {secret_number}.")
 
 if __name__ == "__main__":
     play_game()
